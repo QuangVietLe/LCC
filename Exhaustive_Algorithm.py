0e0e7ba1282e19eaca2d9e1b3d87d242d37640d0
QuangVietLe <74610976+QuangVietLe@users.noreply.github.com>
2021-09-02 15:37:55 +0100

    Update Exhaustive_Algorithm.py

--- Exhaustive_Algorithm.py
+++ Exhaustive_Algorithm.py
@@ -39,4 +39,6 @@ while True:
             else:
                 index = -1
 
-#NOTE: all tuples found are not reflected, like in the Collatz_Tester File. Please do this manually, if you wish to find satisfying tuples. This also does not have file handling nor a programmed timer, so please do this manually, if you wish to do so.
+'''NOTE: all tuples found are not reflected, like in the Collatz_Tester File. 
+Please do this manually, if you wish to find satisfying tuples. 
+This also does not have file handling nor a programmed timer, so please do this manually, if you wish to do so.'''
